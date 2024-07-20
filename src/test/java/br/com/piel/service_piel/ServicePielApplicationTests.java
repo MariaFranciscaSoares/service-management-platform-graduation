b041f3ab1af556fc7ae82ba2e60f213a4d641d17
mfsoares <mariafranciscasoares77@gmail.com>
2024-07-19 21:18:38 -0300

    project created

---
+++ src/test/java/br/com/piel/service_piel/ServicePielApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.piel.service_piel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
